8df7ad76df7e655b9ff3b19f199be29da04c119c
AndriiMishch <anderito02@gmail.com>
2023-07-04 18:33:49 +0200

    Create 01-gallery.js

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector(".gallery");
+createGallery();
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionData: "alt",
+  captionDelay: 250,
+});
+function createGallery() {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" width="800" height="600"/>
+        </a>
+    </li>`;
+    })
+    .join("");
+  galleryList.insertAdjacentHTML("afterbegin", markup);
+} 
+
+galleryList.style.listStyle = 'none'
