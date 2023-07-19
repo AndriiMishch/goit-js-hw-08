73b55df562937b862d80d5d4ae42dd3108ebfc4e
AndriiMishch <anderito02@gmail.com>
2023-07-19 20:29:54 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import '@vimeo/player/dist/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', onPlay);
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onPlay = function(data) {
+    console.log('played the video!')
+};
+
+player.on('timeupdate', function(time) {
+   time = currentTime()
+   console.log(time)
+});
